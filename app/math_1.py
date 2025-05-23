69968e21c914b5088c25c0bd1fb9e6c16c329a0d
endof603 <maksmelnuk99@gmail.com>
2025-05-23 17:56:32 +0300

    Add files via upload

---
+++ app/math_1.py
@@ -0,0 +1,12 @@
+a = int (input("==>"))
+command = input("==>")
+b = int (input("==>"))
+
+if command == '+':
+    print(a + b)
+if command == '-':
+    print(a - b)
+if command == '*':
+    print(a * b)
+if command == '/':
+    print(a / b)
